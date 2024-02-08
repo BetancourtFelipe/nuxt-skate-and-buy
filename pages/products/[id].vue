fa1c901cb639f85f53a327fa857197927a10176f
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-02-08 11:21:16 +0100

    Setup ecommerce pages

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Procuct details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+      consequuntur, dignissimos nemo officia maxime omnis repellat enim. Beatae
+      explicabo blanditiis perferendis incidunt maxime ut alias molestiae, omnis
+      corporis quibusdam necessitatibus.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
